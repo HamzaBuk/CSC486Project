e2785ad912caba34a9a54855e3de5718ba714f87
sc942958 <90327843+sc942958@users.noreply.github.com>
2025-03-26 16:00:08 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,11 @@
 import requests
-from pprint import pprint
 import json
+from dotenv import load_dotenv
+import os
 
-# r = requests.get("https://api.spoonacular.com/recipes/\
-#                  information?apiKey=a8b79bd13e1b4daea84052d93eee4093\
-#                  complexSearch?query=pasta&maxFat=25&number=2")
-r = requests.get("https://api.spoonacular.com/recipes/716429/information?apiKey=a8b79bd13e1b4daea84052d93eee4093&includeNutrition=false&number=1")
+api_key = os.getenv('api_key')
+url = "https://api.spoonacular.com/recipes/716429/information?apiKey="+api_key+"&includeNutrition=false&number=1"
+r = requests.get(url)
 with open("sample.json", "w") as f:
     json.dump(r.json(), f)
 
