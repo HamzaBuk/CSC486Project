dfb020d688db229a61697278d531f08f349ef60b
sc942958 <90327843+sc942958@users.noreply.github.com>
2025-04-21 18:35:04 -0400

    Update server.js

--- jon_test/server.js
+++ jon_test/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth');
 const ingredientRoutes = require('./routes/ingredients');
+const spoonacularRoutes = require('./routes/spoonacular');
 
 app.use(express.json());
 
 // Route imports
 app.use('/auth', authRoutes);           // /auth/register, /auth/login
 app.use('/ingredients', ingredientRoutes); // protected routes
+app.use('/spoonacular', spoonacularRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
