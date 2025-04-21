68c12553a4f5d7fe761938cf40bd96ca19199995
sc942958 <90327843+sc942958@users.noreply.github.com>
2025-04-21 19:08:41 -0400

    Update spoonacular.js

--- jon_test/routes/spoonacular.js
+++ jon_test/routes/spoonacular.js
@@ -1,2 +1,21 @@
 //This is the middleware for calling the spoonacular API
-//Code forthcoming
+//I got this code off of stack overflow, it will likely need changes
+const express = require('express');
+const router = express.router();
+const got = require('got');
+const { pipeline } = require('stream');
+const key = process.env.API_KEY;
+
+router.get('/', function(req, res) {
+  const dataStream = got.stream({
+      uri: 'https://api.spoonacular.com/recipes/complexSearch/information?apiKey=${key}&number=1', //We would want this URL to be flexible in the full application
+  });
+  pipeline(dataStream, res, (err) => {
+      if (err) {
+          console.log(err);
+          res.sendStatus(500);
+      }
+  });
+});
+
+module.exports = router;
