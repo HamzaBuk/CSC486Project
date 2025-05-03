d2133c097c8aa2a156d27d7ce5e4874acc1babde
HamzaBuk <HB991961@wcupa.edu>
2025-05-02 22:01:36 -0400

    Added windows.location to login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,31 +1,31 @@
+const backendURL = `http://${window.location.hostname}:4000`;
+
 const login = () => {
     alert('login called');
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
 
-    input = {
+    const input = {
         username: username,
         password: password
-    }
+    };
 
-    fetch('/auth/login', {method: 'post',
+    fetch(`${backendURL}/auth/login`, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(input)}
+        body: JSON.stringify(input)
+    }).then((res) => res.json()
     ).then((res) => {
-        return res.json()
-    }).then((res => {
-        if(res.token){
+        if (res.token) {
             localStorage.setItem('token', res.token);
             location = "/recipe.html";
-        }
-        else{
+        } else {
             alert("Could not log in.");
         }
-    }));
-
-}
+    });
+};
 
 const register = () => {
     alert("register called");
@@ -33,26 +33,25 @@ const register = () => {
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
 
-    input = {
+    const input = {
         username: username,
         email: email,
         password: password
-    }
+    };
 
-    fetch('/auth/register', {method: 'post',
+    fetch(`${backendURL}/auth/register`, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(input)}
+        body: JSON.stringify(input)
+    }).then((res) => res.json()
     ).then((res) => {
-        return res.json()
-    }).then((res => {
-        if(res.token){
+        if (res.token) {
             localStorage.setItem('token', res.token);
             location = "/recipe.html";
-        }
-        else{
+        } else {
             alert("Could not register.");
         }
-    }));
-}
+    });
+};
